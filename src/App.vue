76deab4efa42a9dea0f8cfe4e38433b82ffdca87
Vicente Gamboa <vicente.gamboa@ug.uchile.cl>
2022-09-01 11:04:44 -0400

    MOD: Se cambia a contexto plebiscito constitucional

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="#7c21d9"
       dark
     >
-      <v-btn text href="https://www.seguimos.cl" target="_blank" class="grey--text">
-        🌳 Seguimos
-      </v-btn>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <a href="https://www.apruebodignidad.cl" target="_blank" class="pa-2">
+            <v-img
+              src="/apruebo-dignidad-logo.png"
+              alt="Logo Apruebo Dignidad"
+              height="50"
+              contain
+              max-width="100"
+            />  
+          </a>
+        </v-col>
+      </v-row>
     </v-app-bar>
     <v-main>
       <Conteo/>
     </v-main>
     <v-footer fixed>
-      <a style="text-decoration: none" href="https://www.seguimos.cl" target="_blank" class="black--text">🌳 Seguimos</a>
+      <a style="text-decoration: none" href="https://conteo.seguimos.cl" target="_blank" class="black--text">
+        Clonada de 🌳 Conteo Seguimos - Desarrolladas por Vicente Gamboa
+      </a>
     </v-footer>
   </v-app>
 </template>
